d3046b95df60626f24ec77ee7682d0ac942f65d8
Nate Lovell <Natelovell99@gmail.com>
2021-06-09 04:07:42 -0600

    Create Identify Files and Folders.jsx

---
+++ Identify Files and Folders.jsx
@@ -0,0 +1,11 @@
+﻿var rootFolder = Folder("~/Desktop");
+
+var files = rootFolder.getFiles();
+
+for(var i = 0; i < files.length; i++) {
+    if(files[i] instanceof File) {
+        $.writeln(files[i].name.replace(/%20/g, " ") + " is a file!");
+        } else if(files[i] instanceof Folder) {
+        $.writeln(files[i].name.replace(/%20/g, " ") + " is a folder!"); 
+            }
+    }
